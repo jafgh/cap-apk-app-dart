b6f1a5f02d901919aef8149f5bf4931004205718
jafgh <jaafar221gh@gmail.com>
2025-04-27 15:05:14 +0300

    Create MainActivity.kt

---
+++ android/app/src/main/kotlin/com/example/captcha_solver_flutter/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.captcha_solver_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // v2 embedding handles everything; لا حاجة لأي overrides
+}
