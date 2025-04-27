7f73fcc05395adb5adfdb6b1104a1428a4231a70
jafgh <jaafar221gh@gmail.com>
2025-04-27 14:50:23 +0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com.example.captcha_solver_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com.example.captcha_solver_flutter/MainActivity.kt
@@ -1,7 +1,7 @@
-package com.example.captcha_solver_flutter
+package com.example.captcha_solver_flutter  // عدّل حسب package name في build.gradle
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    // لا حاجة لأي Override هنا؛ v2 embedding يقوم بكل شيء تلقائياً
+    // لا حاجة لأي overrides لأن v2 embedding يتولّى كل شيء تلقائياً
 }
